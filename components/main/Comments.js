cc5a1fe033f860757bca01748fb31d8863bec77b
nikanlahut <lahutnikan@gmail.com>
2021-05-21 22:14:24 +0300

    commit

---
+++ components/main/Comments.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react'
+import {RefreshControl, View, Text, FlatList, Button, TextInput } from 'react-native'
+
+import firebase from 'firebase'
+require('firebase/firestore')
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+function Comment(props) {
+    const [comments, setComments] = useState([])
+    const [postId, setPostId] = useState("")
+    const [text, setText] = useState("")
+    const {currentUser} = props;
+    const username = currentUser.username
+
+    useEffect(() => {
+
+        /*
+        function matchUserToComment(comments) {
+            for (let i = 0; i < comments.length; i++) {
+                if (comments[i].hasOwnProperty('user')) {
+                    continue;
+                }
+
+                const user = props.users.find(x => x.uid === comments[i].creator)
+                if (user == undefined) {
+                    props.fetchUsersData(comments[i].creator, false)
+                } else {
+                    comments[i].user = user
+                }
+            }
+            setComments(comments)
+        } */
+
+
+        
+            firebase.firestore()
+                .collection('posts')
+                .doc(props.route.params.postId)
+                .collection('comments')
+                .get()
+                .then((snapshot) => {
+                    let comments = snapshot.docs.map(doc => {
+                        const data = doc.data();
+                        const id = doc.id;
+                        return { id, ...data }
+                    })
+                    setComments(comments)
+                })
+            setPostId(props.route.params.postId)
+         /* else {
+            matchUserToComment(comments)
+        } */
+    }, [props.route.params.postId, props.users])
+
+
+    const onCommentSend = () => {
+        firebase.firestore()
+            .collection('posts')
+            .doc(props.route.params.postId)
+            .collection('comments')
+            .add({
+                creator: firebase.auth().currentUser.uid,
+                text,
+                username
+            })
+    }
+
+    return (
+        <View>
+            <FlatList
+                numColumns={1}
+                horizontal={false}
+                data={comments}
+                renderItem={({ item }) => (
+                    <View>
+
+                            <Text>
+                                {item.username}
+                            </Text>
+                        <Text>{item.text}</Text>
+                    </View>
+                )}
+                
+            />
+
+            <View>
+                <TextInput
+                    placeholder='comment...'
+                    onChangeText={(text) => setText(text)} />
+                <Button
+                    onPress={() => onCommentSend()}
+                    title="Send"
+                />
+            </View>
+
+        </View>
+    )
+}
+
+
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser,
+    posts: store.userState.posts,
+})
+//const mapDispatchProps = (dispatch) => bindActionCreators({ fetchUsersData }, dispatch);
+
+export default connect(mapStateToProps)(Comment);
